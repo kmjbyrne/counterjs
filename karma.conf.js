0d35183aabdc2e009cb3397892a8d2e87db66d37
Keith Byrne <keithmbyrne@gmail.com>
2017-10-20 20:08:03 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,24 @@
 // karma.conf.js
 module.exports = function(config) {
     config.set({
-      basePath: './tests',
-      frameworks: ['jasmine'],
-      // frameworks: ['karma-jasmine'],
-      browsers: ['Chrome'],
-      singleRun: true,
-      files: [
+        basePath: './tests',
+        frameworks: ['jasmine'],
+        // frameworks: ['karma-jasmine'],
+        browsers: ['Chrome'],
+        singleRun: true,
+        files: [
           '*.js'
-      ]
-      //...
+        ],
+        customLaunchers: {
+        // tell TravisCI to use chromium when testing
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+        //...
+        // Detect if this is TravisCI running the tests and tell it to use chromium
+        if(process.env.TRAVIS){
+           config.browsers = ['Chrome_travis_ci'];
+        }
     });
-  };
+};

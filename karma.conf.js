19365ba1d32922b69a6409d5ec95945043a39260
Keith Byrne <keithmbyrne@gmail.com>
2017-10-20 19:52:53 +0100

    Create karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,10 @@
+// karma.conf.js
+module.exports = function(config) {
+    config.set({
+      basePath: './tests',
+      frameworks: ['jasmine'],
+      // frameworks: ['karma-jasmine'],
+      // browsers: ['Chrome'],
+      //...
+    });
+  };
